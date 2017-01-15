af3026891684b82e6fe699ab3ce031012d48d761
Nate Kaiser <natejkaiser@gmail.com>
2017-01-15 00:05:28 -0600

    initial ros package setup

---
+++ src/control.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import sys
+sys.dont_write_bytecode = True
+import rospy
+from geometry_msgs.msg import Twist
+import numpy as np
+import roslib
+import time
+
+
+def rockabyebaby():
+    vel_max = 0.3
+    t0 = rospy.get_rostime().to_sec()
+    T = 2
+    while not rospy.is_shutdown():
+        output_command = Twist()
+        t = rospy.get_rostime().to_sec() - t0
+        vel = vel_max * np.sin(np.pi*t/T)
+        output_command.linear.x = vel
+        # output_command.angular.z = dh.w
+        # rospy.loginfo(output_command)
+        pub.publish(output_command)
+
+        rate.sleep()
+
+
+if __name__ == '__main__':
+    rospy.init_node('jackal_control')
+    rate = rospy.Rate(100)
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=5)
+    try:
+        rockabyebaby()
+    except rospy.ROSInterruptException:
+        pass
