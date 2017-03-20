f3f8bb1e1707d1da6132a37fca98bc92c4595c58
Nate Kaiser <natejkaiser@gmail.com>
2017-03-20 15:34:02 -0500

    cleaned up floor_finder and relevant CMakeLists section

--- src/floor_finder.cpp
+++ src/floor_finder.cpp
@@ -2,51 +2,16 @@
 // https://github.com/ritwik1993/pcl_495/blob/master/src/pcl_node_w_nodelets.cpp
 
 #include <ros/ros.h>
-
 #include <sensor_msgs/PointCloud2.h>
-
-#include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/point_cloud.h>
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
-#include <pcl/Vertices.h>
-
 #include <pcl/segmentation/sac_segmentation.h>
-#include <pcl/segmentation/conditional_euclidean_clustering.h>
-#include <pcl/segmentation/impl/conditional_euclidean_clustering.hpp>
-
-#include <pcl/filters/extract_indices.h>
-
-#include <pcl/sample_consensus/ransac.h>
-#include <pcl/sample_consensus/sac_model_plane.h>
-#include <pcl/sample_consensus/model_types.h>
-#include <pcl/sample_consensus/method_types.h>
-
-#include <pcl/ModelCoefficients.h>
-
-#include <pcl/surface/mls.h>
-#include <pcl/surface/concave_hull.h>
-
-#include <pcl/filters/crop_box.h>
-#include <pcl/filters/voxel_grid.h>
-#include <pcl/filters/project_inliers.h>
-#include <pcl/filters/statistical_outlier_removal.h>
-#include <pcl/filters/passthrough.h>
-
-#include <boost/thread/thread.hpp>
 #include <boost/make_shared.hpp>
-
-#include <pcl/features/normal_3d.h>
-#include <pcl/kdtree/kdtree.h>
-#include <pcl/segmentation/extract_clusters.h>
-
 #include <tf/transform_broadcaster.h>
-
 #include <cmath>
 
 
 
-class Floor_Estimater
+class FloorFinder
 {
 private:
   // exercise the demons
@@ -65,17 +30,16 @@ private:
 
 
 public:
-  Floor_Estimater()
+  FloorFinder()
   {
     // create publisher for output point cloud
     pub = nh.advertise<sensor_msgs::PointCloud2>("output", 1);
-    // pub_obj = nh.advertise<sensor_msgs::PointCloud2> ("objects", 1);
 
     // subscribe to input point cloud
-    sub = nh.subscribe("/velodyne_points/voxeled", 1, &Floor_Estimater::cloud_cb, this);
+    sub = nh.subscribe("/velodyne_points/voxeled", 1, &FloorFinder::cloud_cb, this);
 
     // callback to publish ground plane estimate
-    tmr = nh.createTimer(ros::Duration(0.02), &Floor_Estimater::tf_cb, this); // 50 hz to match the rest of the tf tree
+    tmr = nh.createTimer(ros::Duration(0.02), &FloorFinder::tf_cb, this); // 50 hz to match the rest of the tf tree
 
     floor_coefficients = pcl::ModelCoefficients::Ptr(new pcl::ModelCoefficients());
   }
@@ -84,8 +48,6 @@ public:
 private:
   void ransac(const sensor_msgs::PointCloud2ConstPtr& input, pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_projected)
   {
-    // std::cout << "entered ransac() function" << std::endl;
-
     pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
     pcl::PointIndices::Ptr floor_indices(new pcl::PointIndices());
     pcl::fromROSMsg(*input, *cloud);
@@ -103,84 +65,18 @@ private:
     floor_finder.setInputCloud(boost::make_shared<pcl::PointCloud<pcl::PointXYZI> >(*cloud));
     floor_finder.segment(*floor_indices, *floor_coefficients);
     std::cout << *floor_coefficients << std::endl;
-
-
-    // if (floor_indices->indices.size() > 0)
-    // {
-    //   // Extract the floor plane inliers
-    //   pcl::PointCloud<pcl::PointXYZI>::Ptr floor_points(new pcl::PointCloud<pcl::PointXYZI>);
-    //   pcl::ExtractIndices<pcl::PointXYZI> extractor;
-    //   extractor.setInputCloud(boost::make_shared<pcl::PointCloud<pcl::PointXYZI> >(*cloud));
-    //   extractor.setIndices(floor_indices);
-    //   extractor.filter(*floor_points);
-    //   extractor.setNegative(true);
-    //   extractor.filter(*cloud);
-    //
-    //   // Project the floor inliers
-    //   pcl::ProjectInliers<pcl::PointXYZI> proj;
-    //   proj.setModelType(pcl::SACMODEL_PLANE);
-    //   proj.setInputCloud(floor_points);
-    //   proj.setModelCoefficients(floor_coefficients);
-    //   proj.filter(*cloud_projected);
-    //
-    //   floor_points->header.frame_id = "velodyne";
-    //   floor_points->header.stamp = ros::Time::now().toNSec();
-    // }
-
-    // creating a kd tree object for the search method of the extraction
-    // pcl::search::KdTree<pcl::PointXYZI>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZI>);
-    // tree->setInputCloud (boost::make_shared<pcl::PointCloud<pcl::PointXYZI> >(*cloud));
-
-    // new euclidean clustering code
-    // std::vector<pcl::PointIndices> cluster_indices;
-    // pcl::EuclideanClusterExtraction<pcl::PointXYZI> ec;
-    // ec.setClusterTolerance (0.02); // 20cm
-    // ec.setMinClusterSize (100);
-    // ec.setMaxClusterSize (25000);
-    // ec.setSearchMethod (tree);
-    // ec.setInputCloud (boost::make_shared<pcl::PointCloud<pcl::PointXYZI> >(*cloud));
-    // ec.extract (cluster_indices);
-    // std::cout << *floor_coefficients << std::endl;
-
-
-    // int j = 0;
-    //
-    // for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
-    // {
-    //   pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZI>);
-    //   for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); pit++)
-    //   {
-    //     cloud_cluster->points.push_back (cloud_projected->points[*pit]); //*
-    //   }
-    //   cloud_cluster->width = cloud_cluster->points.size ();
-    //   cloud_cluster->height = 1;
-    //   cloud_cluster->is_dense = true;
-    //   cloud_cluster->header.frame_id = "camera_link";
-    //   cloud_cluster->header.stamp = ros::Time::now().toNSec();
-    //   pub_obj.publish(*cloud_cluster);
-    //   //std::cout << "PointCloud representing the Cluster: " << cloud_cluster->points.size () << " data points." << std::endl;
-    //   //std::stringstream ss;
-    //   //ss << "cloud_cluster_" << j << ".pcd";
-    //   j++;
-    // }
-
   }
 
   void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
   {
-    // std::cout << "entered cloud callback" << std::endl;
-
     // run ransac to find floor
     pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_projected(new pcl::PointCloud<pcl::PointXYZI>);
     ransac(input, cloud_projected);
     pub.publish(*cloud_projected);
-    // std::cout << "width " << cloud_projected->width << std::endl;
   }
 
   void tf_cb(const ros::TimerEvent& event)
   {
-    // std::cout << "entered tf callback" << std::endl;
-
     // calculate ground plane triax
     if(floor_coefficients->values.size())
     {
@@ -203,7 +99,7 @@ private:
     }
   }
 
-}; // END OF Floor_Estimater CLASS
+}; // END OF FloorFinder CLASS
 
 
 
@@ -213,8 +109,7 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "floor_estimater");
 
   // instantiate class for storing/accessing tf data
-  Floor_Estimater flr_est;
+  FloorFinder flr_est;
 
-  // sit and:
   ros::spin();
 }
